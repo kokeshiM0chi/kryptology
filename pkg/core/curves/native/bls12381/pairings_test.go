ac80ebd0b1b74b5c8f30dc0ca1040006045b26f0
kekeshiM0chi <frogandfrog009@gmail.com>
2022-07-18 18:22:31 +0900

    feat: add the test bilinear map property

--- pkg/core/curves/native/bls12381/pairings_test.go
+++ pkg/core/curves/native/bls12381/pairings_test.go
@@ -4,6 +4,7 @@ import (
 	crand "crypto/rand"
 	"testing"
 
+	"fmt"
 	"github.com/stretchr/testify/require"
 
 	"github.com/coinbase/kryptology/pkg/core/curves/native"
@@ -12,13 +13,19 @@ import (
 func TestSinglePairing(t *testing.T) {
 	g := new(G1).Generator()
 	h := new(G2).Generator()
+	fmt.Println("# g:	", g)
+	fmt.Println("# h:	", h)
 
 	e := new(Engine)
+	fmt.Println("# pairin　before reset:	", e.Result())
 	e.AddPair(g, h)
 	p := e.Result()
+	fmt.Println("# pairing:	", e.Result())
+
 	p.Neg(p)
 
 	e.Reset()
+	fmt.Println("# pairin　after reset:	", e.Result())
 	e.AddPairInvG2(g, h)
 	q := e.Result()
 	e.Reset()
@@ -61,4 +68,38 @@ func TestMultiPairing(t *testing.T) {
 
 	actual := e2.Result()
 	require.Equal(t, 1, expected.Equal(actual))
+}
+
+// confirm
+// e(a * g1, g2) == e(g1, a * g2)
+func TestCoefficients(t *testing.T) {
+	e := new(Engine)
+	Bls12381FqNew()
+	// setting scalar value
+	var bytesA [64]byte
+
+	_, _ = crand.Read(bytesA[:])
+	var a *native.Field
+
+	a = Bls12381FqNew()
+	a.SetBytesWide(&bytesA)
+
+	// generate base point
+	g1 := new(G1).Generator()
+	g2 := new(G2).Generator()
+
+	g1.Mul(g1, a)
+
+	e.AddPair(g1, g2)
+	gt0 := e.Result()
+
+	e.Reset()
+	g1 = new(G1).Generator()
+	g2 = new(G2).Generator()
+	g2.Mul(g2, a)
+
+	e.AddPair(g1, g2)
+	gt1 := e.Result()
+
+	require.Equal(t, gt1.Bytes(), gt0.Bytes())
 }
